41f56df1b97480ac49e12043b7277228c0ae3dce
Pete Windle <git@pete23.com>
2019-12-05 21:25:10 +0000

    implement input and output functions

--- src/intcode.clj
+++ src/intcode.clj
@@ -23,25 +23,36 @@
     (assoc program dst-loc result)))
 
 (defprotocol Machine
-  (decode-instruction [_])
   (run-step [_])
   (run-machine [_]))
 
+  
 (defrecord IntcodeMachine [pc program input output]
   Machine
 
-  (decode-instruction [machine]
-      (let [[opcode src1 src2 dst] (subvec program pc)
-            operation (case opcode 1 + 2 *)]
-        (fn [m] (IntcodeMachine. (+ 4 (:pc m))
-                                 (assoc program dst (operation (nth program src1) (nth program src2)))
-                                 (:input m)
-                                 (:output m)))))
-
   (run-step [machine]
-    (let [instruction (decode-instruction machine)]
-      (instruction machine)))
-  
+    (let [instruction (subvec program pc)
+          opcode (first instruction)]
+      (case opcode
+        ;; 1 + 2 -
+        (1 2) (let [[_ src1 src2 dst] instruction
+                    operation (case opcode 1 + 2 *)]
+                (IntcodeMachine. (+ 4 pc)
+                                 (assoc program dst (operation (nth program src1) (nth program src2)))
+                                 input
+                                 output))
+        ;; 3 input
+        3 (let [[_ dst] instruction]
+            (IntcodeMachine. (+ 2 pc)
+                             (assoc program dst (first input))
+                             (rest input)
+                             output))
+        ;; 4 output
+        4 (let [[_ src] instruction]
+            (IntcodeMachine. (+ 2 pc)
+                             program
+                             input
+                             (conj output (nth program src)))))))
   (run-machine [machine]
     (loop [m machine]
       (if (= 99 (nth (:program m) (:pc m)))
@@ -49,10 +60,15 @@
         (recur (run-step m))))))
         
 (defn run-program
-  ([program] (:program (run-machine (IntcodeMachine. 0 program [] [])))))
+  ([program] (:program (run-machine (IntcodeMachine. 0 program [] []))))
+  ([program input] (run-machine (IntcodeMachine. 0 program input []))))
 
-(deftest test-interpreter
+(deftest test-interpreter-addition-multiplication-exit
   (is (= (run-program [1 0 0 0 99]) [2 0 0 0 99]))
   (is (= (run-program [2 3 0 3 99]) [2 3 0 6 99]))
   (is (= (run-program [2 4 4 5 99 0]) [2 4 4 5 99 9801]))
   (is (= (run-program [1 1 1 4 99 5 6 0 99]) [30 1 1 4 2 5 6 0 99])))
+
+(deftest test-io
+  (is (= (run-program [3 0 4 0 99] [-999])
+         (IntcodeMachine. 4 [-999 0 4 0 99] [] [-999]))))
